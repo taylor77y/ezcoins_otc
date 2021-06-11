0b788e15e1896ce87fa102c1a04042fdb617185c
wanglei <1044508403@qq.com>
2021-06-11 11:12:26 +0800

    Initial commit

---
+++ src/main/java/com/ezcoins/EzcoinsParentApplication.java
@@ -0,0 +1,13 @@
+package com.ezcoins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzcoinsParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzcoinsParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ezcoins/EzcoinsParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezcoins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzcoinsParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
