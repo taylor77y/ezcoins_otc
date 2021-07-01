0ceba8276f40be31dfc1a6a8b84c64a99869ce01
wanglei <1044508403@qq.com>
2021-07-01 15:45:27 +0800

    OTC 业务 2.0

---
+++ src/main/java/com/ezcoins/project/otc/entity/resp/AdvertisingBusinessInfoRespDto.java
@@ -0,0 +1,43 @@
+package com.ezcoins.project.otc.entity.resp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @Author: WangLei
+ * @Email: 1044508403@qq.com
+ * @Description:
+ * @Date:2021/7/1 14:43
+ * @Version:1.0
+ */
+@Data
+public class AdvertisingBusinessInfoRespDto {
+    @ApiModelProperty(value = "昵称")
+    private String advertisingName;
+
+    @ApiModelProperty(value = "卖单数量")
+    private Integer sellCount;
+
+    @ApiModelProperty(value = "买单数量")
+    private Integer buyCount;
+
+    @ApiModelProperty(value = "平均放行时间 分钟")
+    private Double mouthAveragePass;
+
+    @ApiModelProperty(value = "30 日成单")
+    private Integer mouthCount;
+
+    @ApiModelProperty(value = "注册时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date registrationTime;
+
+    @ApiModelProperty(value = "实名认证状态 状态：0:已认证 1：未认证 ")
+    private String kycStatus;
+
+    @ApiModelProperty(value = "高级认证状态 状态：0:已认证 1：未认证 ")
+    private String advertisingStatus;
+
+}
