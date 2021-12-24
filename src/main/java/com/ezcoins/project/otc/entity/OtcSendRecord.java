bd42e9b9f9a90f082db6af5dd49912e186f03ead
taylor <taylor77y@gmail.com>
2021-12-24 10:17:38 -0800

    根据OTC交易word文档新增次级菜单-订单发送记录实体类

---
+++ src/main/java/com/ezcoins/project/otc/entity/OtcSendRecord.java
@@ -0,0 +1,37 @@
+package com.ezcoins.project.otc.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ *       次级菜单-OTC订单
+ * </p>
+ *
+ * @author taylor
+ * @since 2021-12-15
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("otc_send_record")
+@ApiModel(value="OtcSendRecord对象", description="次级菜单-订单发送记录")
+public class OtcSendRecord {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "otc交易id")
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    private String id;
+
+    @ApiModelProperty(value = "发送类型")
+    //发起订单、买方已付款、卖方确认中、已取消、买卖方确认完成、平台转账成功
+    private String send_type;
+
+    @ApiModelProperty(value = "时间")
+    private String send_time;
+}
