8ad4b6cc2548d02f558491ac1f9b3b2138b95180
taylor <taylor77y@gmail.com>
2021-12-24 10:52:50 -0800

    添加selectUserBy的单元测试方法

--- src/test/java/com/ezcoins/EzcoinsParentApplicationTests.java
+++ src/test/java/com/ezcoins/EzcoinsParentApplicationTests.java
@@ -1,19 +1,16 @@
 package com.ezcoins;
 
-import com.ezcoins.project.coin.udun.Address;
-import com.ezcoins.project.coin.udun.ResponseMessage;
 import com.ezcoins.project.coin.wallet.cc.WalletClientService;
 import com.ezcoins.project.common.service.EmailService;
 import com.ezcoins.project.common.service.MailFeedbackService;
-import com.ezcoins.project.otc.service.EzOtcOrderIndexService;
-import com.ezcoins.utils.HttpUtils;
+import com.ezcoins.project.consumer.entity.EzUser;
+import com.ezcoins.project.consumer.mapper.EzUserMapper;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.math.BigDecimal;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -34,6 +31,17 @@ class EzcoinsParentApplicationTests {
 //        Address address = walletClient.createAddressList("test");
 //        walletClient.transfer("1","1","1","0x58d9e6d57ce65ef5dcfc1b22022c7a866573e99b","test", BigDecimal.ONE);
 //        System.out.println(address);
+    }
+
+    @Autowired
+    private EzUserMapper ezUserMapper;
+
+    @Test
+    public void testSelectUserBy_whenSelect_thenOk() {
+        System.out.println(("----- selectAll method test ------"));
+        EzUser user = ezUserMapper.selectUserBy("delores5566@gmail.com", "15889789210", "shenzhen", "delores5566@gmail.com", "q013P2UiP1");
+        Assert.assertEquals("15889789210", user.getPhone());
+//        userList.forEach(System.out::println);
     }
 
 }
