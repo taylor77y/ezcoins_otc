f8559a704fe2697329953a60fed2d4224e4434b3
taylor <taylor77y@gmail.com>
2021-12-25 19:59:48 -0800

    给数据库表对应的实体类加上validator注解

--- src/main/java/com/ezcoins/project/otc/entity/EzInternetAccount.java
+++ src/main/java/com/ezcoins/project/otc/entity/EzInternetAccount.java
@@ -8,41 +8,51 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
  * <p>
- *       支付、微信、银行卡都在此表
+ *       网络账号表
  * </p>
  *
- * @author wanglei
- * @since 2021-06-15
+ * @author taylor
+ * @since 2021-12-15
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
 @TableName("ez_internet_account")
-@ApiModel(value="EzInternetAccount对象", description="")
+@ApiModel(value="EzInternetAccount对象", description="网络账号表")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class EzInternetAccount extends BaseEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "银行信息id")
+    @ApiModelProperty(value = "id")
     @TableId(value = "id", type = IdType.ASSIGN_ID)
     private String id;
 
-    @ApiModelProperty(value = "用户id")
+    @ApiModelProperty(value = "用户id",required = true)
+    @NotBlank
     private String userId;
 
-    @ApiModelProperty(value = "网络账号姓名")
+    @ApiModelProperty(value = "网络账号名",required = true)
+    @NotBlank
     private String internetAccountName;
 
-    @ApiModelProperty(value = "网络账号号码")
+    @ApiModelProperty(value = "网络账号号码",required = true)
+    @NotBlank
+    @Size(min=1, max = 32)
     private String internetAccountNumber;
 
-    @ApiModelProperty(value = "网络账号类型")
-    private String internetAccountType;
+    @ApiModelProperty(value = "网络账号类型 0微信 1QQ 2支付宝 3微博 4抖音 5陌陌 6豆瓣 7百度贴吧 8小红书 9优酷 10大众点评 11钉钉 12华为id 13apple id",required = true)
+    private short internetAccountType;
 
-    @ApiModelProperty(value = "状态(0:已激活 1：未激活)")
-    private String status;
+    @ApiModelProperty(value = "状态(0:已激活 1：未激活)",required = true)
+    @NotBlank(message = "请先选择状态")
+    private short status;
 }
--- src/main/java/com/ezcoins/project/otc/entity/EzPaymentBank.java
+++ src/main/java/com/ezcoins/project/otc/entity/EzPaymentBank.java
@@ -1,14 +1,19 @@
 package com.ezcoins.project.otc.entity;
 
-import com.baomidou.mybatisplus.annotation.*;
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.ezcoins.base.BaseEntity;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
-import java.util.Date;
 
 /**
  * <p>
@@ -21,34 +26,47 @@ import java.util.Date;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @TableName("ez_payment_bank")
-@ApiModel(value="EzPaymentBank对象", description="")
-public class EzPaymentBank implements Serializable {
+@ApiModel(value="EzPaymentBank对象", description="用户名下的银行卡")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class EzPaymentBank extends BaseEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "银行信息id")
+    @ApiModelProperty(value = "id")
     @TableId(value = "id", type = IdType.ASSIGN_ID)
     private String id;
 
-    @ApiModelProperty(value = "用户id")
+    @ApiModelProperty(value = "用户id",required = true)
+    @Size(min=1, max = 32)
+    @NotBlank
     private String userId;
 
-    @ApiModelProperty(value = "卖家姓名")
+    @ApiModelProperty(value = "卖家姓名",required = true)
+    @Size(min=1, max = 32)
     private String realName;
 
-    @ApiModelProperty(value = "银行名")
+    @ApiModelProperty(value = "银行名",required = true)
+    @Size(min=1, max = 32)
     private String bankName;
 
-    @ApiModelProperty(value = "账户名")
+    @ApiModelProperty(value = "银行英文缩写编码",required = true)
+    @Size(min=1, max = 10)
+    @NotBlank
+    private String bankCode;
+
+    @ApiModelProperty(value = "账户名",required = true)
+    @Size(min=1, max = 32)
     private String accountName;
 
-    @ApiModelProperty(value = "账户号")
+    @ApiModelProperty(value = "银行卡号",required = true)
+    @Size(min=1, max = 32)
+    @NotBlank
     private String number;
 
-    @ApiModelProperty(value = "状态(0:已激活 1：未激活)")
-    private String status;
+    @ApiModelProperty(value = "状态(0:已激活 1：未激活)",required = true)
+    private short status;
 
-    @ApiModelProperty(value = "创建时间")
+/*    @ApiModelProperty(value = "创建时间")
     @TableField(fill = FieldFill.INSERT)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
@@ -56,6 +74,6 @@ public class EzPaymentBank implements Serializable {
     @ApiModelProperty(value = "更新时间")
     @TableField(fill = FieldFill.INSERT_UPDATE)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date updateTime;
+    private Date updateTime;*/
 
 }
