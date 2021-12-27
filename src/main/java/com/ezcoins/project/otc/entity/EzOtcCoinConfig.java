d1ed34b142cabb6120ea4a03507fd90b703a5c72
taylor <taylor77y@gmail.com>
2021-12-26 21:51:15 -0800

    更新字段上的注释

--- src/main/java/com/ezcoins/project/otc/entity/EzOtcCoinConfig.java
+++ src/main/java/com/ezcoins/project/otc/entity/EzOtcCoinConfig.java
@@ -2,6 +2,7 @@ package com.ezcoins.project.otc.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -11,7 +12,7 @@ import java.io.Serializable;
 
 /**
  * <p>
- * 订单申诉
+ *      所有数字货币挂单配置表
  * </p>
  *
  * @author wanglei
@@ -19,12 +20,13 @@ import java.io.Serializable;
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-@ApiModel(value="EzOtcCoinConfig对象", description="订单申诉")
+@TableName("ez_otc_coin_config")
+@ApiModel(value="EzOtcCoinConfig对象", description="所有 coin 挂单配置信息表")
 public class EzOtcCoinConfig implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "申诉id")
+    @ApiModelProperty(value = "id")
     @TableId(value = "id", type = IdType.ASSIGN_ID)
     private int id;
 
@@ -43,18 +45,18 @@ public class EzOtcCoinConfig implements Serializable {
     @ApiModelProperty(value = "卖出广告最低发布数量")
     private double minTranscationAmount;
 
-    @ApiModelProperty(value = "申诉id")
+    @ApiModelProperty(value = "最大交易数量")
     private double maxTranscationAmount;
 
     @ApiModelProperty(value = "手续费")
     private double fee;
 
-    @ApiModelProperty(value = "申诉id")
-    private int status;
+    @ApiModelProperty(value = "状态 （0启用 1禁用 ）")
+    private short status;
 
-    @ApiModelProperty(value = "申诉id")
+    @ApiModelProperty(value = "停止销售（0启用 1禁用 ）")
     private int stopSelling;
 
-    @ApiModelProperty(value = "申诉id")
+    @ApiModelProperty(value = "禁止购买（0启用 1禁用 ）")
     private int prohibitionPurchase;
 }
--- src/main/java/com/ezcoins/project/otc/entity/EzOtcCoinInfo.java
+++ src/main/java/com/ezcoins/project/otc/entity/EzOtcCoinInfo.java
@@ -2,33 +2,35 @@ package com.ezcoins.project.otc.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@TableName("ez_internet_account")
 @ApiModel(value="EzOtcCoinInfo对象", description="所有可用 coin 信息")
 public class EzOtcCoinInfo {
 
-    @ApiModelProperty(value = "申诉id")
+    @ApiModelProperty(value = "id")
     @TableId(value = "id", type = IdType.ASSIGN_ID)
     private int id;
 
-    @ApiModelProperty(value = "币种id")
+    @ApiModelProperty(value = "数字货币中文名",required = true)
     private String name;
 
-    @ApiModelProperty(value = "币种id")
+    @ApiModelProperty(value = "数字货币英文名",required = true)
     private String englishName;
 
-    @ApiModelProperty(value = "币种id")
+    @ApiModelProperty(value = "数字货币英文缩写名",required = true)
     private String englishShortName;
 
-    @ApiModelProperty(value = "币种id")
+    @ApiModelProperty(value = "数字货币类型id")
     private int coinTypeId;
 
-    @ApiModelProperty(value = "币种id")
+    @ApiModelProperty(value = "数字货币类型名")
     private String coinTypeName;
 
-    @ApiModelProperty(value = "币种id")
+    @ApiModelProperty(value = "类型")
     private String type;
 }
